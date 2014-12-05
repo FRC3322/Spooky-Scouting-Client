12128d3684fd701febe93ab9149f55fa58d486bc
AdrS <AdrS1118@gmail.com>
2014-12-04 19:34:35 -0500

    modified placement next back buttons

--- app/src/main/java/com/example/frc3322_04/scoutingclient/Scout.java
+++ app/src/main/java/com/example/frc3322_04/scoutingclient/Scout.java
@@ -1,34 +1,22 @@
 package com.example.frc3322_04.scoutingclient;
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.text.method.DigitsKeyListener;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
-import android.widget.Spinner;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import com.example.frc3322_04.scoutingclient.*;
 
 import java.io.Serializable;
-import java.text.Normalizer;
 import java.util.ArrayList;
 
 public class Scout extends Activity {
 
-    ScrollView scrollView;
+    LinearLayout container;
     ArrayList<FormPage> formPages;
     int currentPage;
     Button back;
@@ -37,18 +25,55 @@ public class Scout extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scout);
-        scrollView = (ScrollView)findViewById(R.id.scouting_scroll);
+        container = (LinearLayout)findViewById(R.id.scouting_container);
         back = (Button)findViewById(R.id.back_button);
         next = (Button)findViewById(R.id.next_button);
         formPages = new ArrayList<FormPage>();
-        FormPage p1 = new FormPage(scrollView.getContext());
+        FormPage p1 = new FormPage(container.getContext());
         p1.add(new NumberBox(p1.getContext(),"Match Number","matchNum",1,true,true,1000, 1));
         String[] opts = {"red","blue"};
         p1.add(new OptionPicker(p1.getContext(),"Choose","Optpick",opts));
         p1.add(new MapCordinatePicker(p1.getContext(),"autonPossition"));
         p1.add(new CheckBox(p1.getContext(),"Auton move","autonMove",false));
+        p1.add(new CheckBox(p1.getContext(),"Auton move","autonMove",false));
+        p1.add(new CheckBox(p1.getContext(),"Auton move","autonMove",false));
+        p1.add(new CheckBox(p1.getContext(),"Auton move","autonMove",false));
+        p1.add(new CheckBox(p1.getContext(),"Auton move","autonMove",false));
+        p1.add(new CheckBox(p1.getContext(),"Auton move","autonMove",false));
+        p1.add(new CheckBox(p1.getContext(),"Auton move","autonMove",false));
+        p1.add(new CheckBox(p1.getContext(),"Auton move","autonMove",false));
+        p1.add(new CheckBox(p1.getContext(),"Auton move","autonMove",false));
+        p1.add(new CheckBox(p1.getContext(),"Auton move","autonMove",false));
+        p1.add(new CheckBox(p1.getContext(),"Auton move","autonMove",false));
+        p1.add(new CheckBox(p1.getContext(),"Auton move","autonMove",false));
+        p1.add(new CheckBox(p1.getContext(),"Auton move","autonMove",false));
+        p1.add(new CheckBox(p1.getContext(),"Auton move","autonMove",false));
+        p1.add(new CheckBox(p1.getContext(),"Auton move","autonMove",false));
+        p1.add(new CheckBox(p1.getContext(),"Auton move","autonMove",false));
+        p1.add(new CheckBox(p1.getContext(),"Auton move","autonMove",false));
+        p1.add(new CheckBox(p1.getContext(),"Auton move","autonMove",false));
+        p1.add(new CheckBox(p1.getContext(),"Auton move","autonMove",false));
+        p1.add(new CheckBox(p1.getContext(),"Auton move","autonMove",false));
+        p1.add(new CheckBox(p1.getContext(),"Auton move","autonMove",false));
+        p1.add(new CheckBox(p1.getContext(),"Auton move","autonMove",false));
+        p1.add(new CheckBox(p1.getContext(),"Auton move","autonMove",false));
+        p1.add(new CheckBox(p1.getContext(),"Auton move","autonMove",false));
+        p1.add(new CheckBox(p1.getContext(),"Auton move","autonMove",false));
+        p1.add(new CheckBox(p1.getContext(),"Auton move","autonMove",false));
+        p1.add(new CheckBox(p1.getContext(),"Auton move","autonMove",false));
+        p1.add(new CheckBox(p1.getContext(),"Auton move","autonMove",false));
+        p1.add(new CheckBox(p1.getContext(),"Auton move","autonMove",false));
+        p1.add(new CheckBox(p1.getContext(),"Auton move","autonMove",false));
+        p1.add(new CheckBox(p1.getContext(),"Auton move","autonMove",false));
+        p1.add(new CheckBox(p1.getContext(),"Auton move","autonMove",false));
+        p1.add(new CheckBox(p1.getContext(),"Auton move","autonMove",false));
+        p1.add(new CheckBox(p1.getContext(),"Auton move","autonMove",false));
+        p1.add(new CheckBox(p1.getContext(),"Auton move","autonMove",false));
+        p1.add(new CheckBox(p1.getContext(),"Auton move","autonMove",false));
+        p1.add(new CheckBox(p1.getContext(),"Auton move","autonMove",false));
+        p1.add(new CheckBox(p1.getContext(),"Auton move","autonMove",false));
         formPages.add(p1);
-        FormPage p2 = new FormPage(scrollView.getContext());
+        FormPage p2 = new FormPage(container.getContext());
         p2.add(new NumberBox(p1.getContext(),"Possesion","possesion",0,false,true,100, 0));
         p2.add(new NumberBox(p1.getContext(),"Truss throw success","trussThrowSuccess",0,false,true,0, 0));
         p2.add(new NumberBox(p1.getContext(),"Truss throw miss","trussThrowMiss",0,false,true,100, 0));
@@ -58,12 +83,12 @@ public class Scout extends Activity {
         p2.add(new NumberBox(p1.getContext(),"Inbounds","inbounds",0,false,true,100, 0));
         p2.add(new NumberBox(p1.getContext(),"Drops","drops",0,false,true,100, 0));
         formPages.add(p2);
-        FormPage p3 = new FormPage(scrollView.getContext());
+        FormPage p3 = new FormPage(container.getContext());
         p3.add(new CheckBox(p3.getContext(),"Fail","fail",false));
         p3.add(new TextArea(p3.getContext(),"Notes","notes",null));
         formPages.add(p3);
         if(formPages.size() > 0)
-            scrollView.addView(formPages.get(0));
+            container.addView(formPages.get(0));
         currentPage = 0;
         fixLabels();
     }
@@ -84,9 +109,9 @@ public class Scout extends Activity {
     }
     public void next(View view) {
         if(currentPage < formPages.size() - 1) {
-            scrollView.removeAllViews();
+            container.removeAllViews();
             currentPage++;
-            scrollView.addView(formPages.get(currentPage));
+            container.addView(formPages.get(currentPage));
         } else {
             ArrayList<Tuple<String, Serializable> > values = new ArrayList<Tuple<String, Serializable> >();
             boolean isFilled = true;
@@ -110,9 +135,9 @@ public class Scout extends Activity {
     public void back(View view) {
         getValues();
         if(currentPage > 0) {
-            scrollView.removeAllViews();
+            container.removeAllViews();
             currentPage--;
-            scrollView.addView(formPages.get(currentPage));
+            container.addView(formPages.get(currentPage));
             fixLabels();
         }
     }
